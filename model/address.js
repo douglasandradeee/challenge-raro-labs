21782ae1e07c5ae1328e53edd7152f2dcd6e51bb
Douglas Andrade <dba12@hotmail.com>
2021-07-16 18:47:52 -0300

    add model

---
+++ model/address.js
@@ -0,0 +1,14 @@
+const address = {
+    cep: 01001-000,
+  logradouro: "Praça da Sé",
+  complemento: "lado ímpar",
+  bairro: "Sé",
+  localidade: "São Paulo",
+  uf: "SP",
+  ibge: "3550308",
+  gia: "1004",
+  ddd: "11",
+  siafi: "7107"
+}
+
+module.exports = address
